77ffa94269e3191c9538e12c3faadcef3c865321
Giulia <giuli4994@gmail.com>
2023-03-24 19:55:30 +0100

    improvements to nav drawer - not working yet

--- app/src/main/java/com/example/mattatoyomng/MainActivity.kt
+++ app/src/main/java/com/example/mattatoyomng/MainActivity.kt
@@ -5,16 +5,24 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.mattatoyomng.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+//    navigation bar toggle
     lateinit var toggle: ActionBarDrawerToggle
+
+    lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        drawerLayout = binding.drawerLayout
         binding.apply {
             toggle = ActionBarDrawerToggle(
                 this@MainActivity, drawerLayout,
@@ -26,9 +34,10 @@ class MainActivity : AppCompatActivity() {
             supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
             navView.setNavigationItemSelectedListener {
+                it.isChecked = true
                 when (it.itemId) {
                     R.id.eventsItem -> {
-                        Toast.makeText(this@MainActivity, "First Item Clicked", Toast.LENGTH_SHORT).show()
+                        replaceFragment(CalendarFragment(), it.title.toString())
                     }
                     R.id.searchItem -> {
                         Toast.makeText(this@MainActivity, "Second Item Clicked", Toast.LENGTH_SHORT).show()
@@ -44,9 +53,17 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun replaceFragment(fragment: Fragment, title: String){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.commit()
+        drawerLayout.closeDrawers()
+        setTitle(title)
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (toggle.onOptionsItemSelected(item)){
-            true
+            return true
         }
         return super.onOptionsItemSelected(item)
     }
