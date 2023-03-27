2b18be2d96ee8d3b7aea1bbb04cf143697baeeb1
Giulia <giuli4994@gmail.com>
2023-03-27 19:27:07 +0200

    working nav drawer

--- app/src/main/java/com/example/mattatoyomng/MainActivity.kt
+++ app/src/main/java/com/example/mattatoyomng/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.example.mattatoyomng
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.example.mattatoyomng.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var binding: ActivityMainBinding
 
-//    navigation bar toggle
+    //    navigation bar toggle
     lateinit var toggle: ActionBarDrawerToggle
 
     lateinit var drawerLayout: DrawerLayout
@@ -22,49 +23,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         drawerLayout = binding.drawerLayout
-        binding.apply {
-            toggle = ActionBarDrawerToggle(
-                this@MainActivity, drawerLayout,
-                R.string.open, R.string.close
-            )
-            drawerLayout.addDrawerListener(toggle)
-            toggle.syncState()
 
-            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle = ActionBarDrawerToggle(
+            this@MainActivity, drawerLayout,
+            R.string.open, R.string.close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
-            navView.setNavigationItemSelectedListener {
-                it.isChecked = true
-                when (it.itemId) {
-                    R.id.eventsItem -> {
-                        replaceFragment(CalendarFragment(), it.title.toString())
-                    }
-                    R.id.searchItem -> {
-                        Toast.makeText(this@MainActivity, "Second Item Clicked", Toast.LENGTH_SHORT).show()
-                    }
-                    R.id.todoItem -> {
-                        Toast.makeText(this@MainActivity, "third Item Clicked", Toast.LENGTH_SHORT).show()
-                    }
-                    R.id.profileItem -> {
-                        Toast.makeText(this@MainActivity, "third Item Clicked", Toast.LENGTH_SHORT).show()
-                    }
-                }
-                true
-            }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navView = binding.navView
+        navView.setNavigationItemSelectedListener(this)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, CalendarFragment()).commit()
+            navView.setCheckedItem((R.id.calendarItem))
         }
     }
 
-    private fun replaceFragment(fragment: Fragment, title: String){
+    private fun replaceFragment(fragment: Fragment, title: String) {
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
         fragmentTransaction.commit()
         drawerLayout.closeDrawers()
         setTitle(title)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
+        if (toggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        item.isChecked = true
+        when (item.itemId) {
+            R.id.calendarItem -> {
+                replaceFragment(CalendarFragment(), item.title.toString())
+            }
+            R.id.searchItem -> {
+                Toast.makeText(this@MainActivity, "search Item Clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            R.id.todoItem -> {
+                Toast.makeText(this@MainActivity, "todo Item Clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }
+            R.id.profileItem -> {
+                Toast.makeText(this@MainActivity, "profile Item Clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+        return true
     }
 }
