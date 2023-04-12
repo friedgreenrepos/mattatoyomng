1016996bc3ee4d7aa724763c7d278e7b1b4f64b5
Giulia <giuli4994@gmail.com>
2023-04-12 23:52:23 +0200

    parcelize user model

--- app/src/main/java/com/example/mattatoyomng/models/User.kt
+++ app/src/main/java/com/example/mattatoyomng/models/User.kt
@@ -1,11 +1,9 @@
 package com.example.mattatoyomng.models
 
-import android.os.Parcel
 import android.os.Parcelable
-import android.widget.ImageView
-import androidx.databinding.BindingAdapter
-import com.bumptech.glide.Glide
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class User(
     val userid: String = "",
     val name: String = "",
@@ -13,30 +11,4 @@ data class User(
     val email: String = "",
     val profilePic: String = "",
     val admin: Boolean = false
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readString()!!,
-        parcel.readByte() != 0.toByte()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(userid)
-        parcel.writeString(name)
-        parcel.writeString(username)
-        parcel.writeString(email)
-        parcel.writeString(profilePic)
-        parcel.writeByte(if (admin) 1 else 0)
-    }
-
-    override fun describeContents() = 0
-
-    companion object CREATOR : Parcelable.Creator<User> {
-        override fun createFromParcel(parcel: Parcel): User = User(parcel)
-
-        override fun newArray(size: Int): Array<User?> = arrayOfNulls(size)
-    }
-}
+) : Parcelable
